f8566d0932c96bba833814e0f1f0a99104da98b1
Stephen Olsen <steve.olsen@gmail.com>
2013-04-02 16:45:01 -0400

    package

--- init.el
+++ init.el
@@ -20,8 +20,11 @@
                       ack-and-a-half
                       ;; Misc.
                       markdown-mode
-                      twilight-theme
-                      hlinum)
+                      color-theme-solarized
+                      hlinum
+                      ;; other stuffs I need
+                      magit
+                      )
   "A list of packages to ensure are installed at launch.")
 
 ;; Automaticaly install any missing packages
@@ -38,5 +41,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(font-lock-warning-face ((t (:inherit nil :foreground "red" :background nil))))
- '(linum-highlight-face ((t (:inherit default :background "color-238" :foreground "white"))))
+ '(linum-highlight-face ((t (:inherit default :background "color-238" :foreground "white"))) t)
  '(show-paren-match ((((class color) (background dark)) (:inherit nil :foreground "red")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" default))))
